67219c1c42c04bfbcebc132bc45c16fa401caa34
perdasilva <perdasilva@redhat.com>
2022-10-24 11:21:44 +0200

    Add solver package with interfaces and basic implementations
    Signed-off-by: perdasilva <perdasilva@redhat.com>

---
+++ internal/solver/solver.go
@@ -0,0 +1,72 @@
+package solver
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/operator-framework/deppy/internal/constraints"
+	"github.com/operator-framework/deppy/internal/entitysource"
+	"github.com/operator-framework/deppy/internal/sat"
+)
+
+var _ Solver = &DeppySolver{}
+
+type Solution map[entitysource.EntityID]bool
+
+type Solver interface {
+	Solve(ctx context.Context) (Solution, error)
+}
+
+type Options struct {
+	EntitySources     []entitysource.EntitySource
+	ConstraintBuilder constraints.ConstraintBuilder
+}
+
+type DeppySolver struct {
+	entitySources     []entitysource.EntitySource
+	constraintBuilder constraints.ConstraintBuilder
+}
+
+func NewDeppySolver(opts Options) (*DeppySolver, error) {
+	if opts.EntitySources == nil {
+		return nil, fmt.Errorf("no entity sources defined for solver")
+	}
+	if opts.ConstraintBuilder == nil {
+		return nil, fmt.Errorf("no constraint generators defined for solver")
+	}
+	return &DeppySolver{
+		entitySources:     opts.EntitySources,
+		constraintBuilder: opts.ConstraintBuilder,
+	}, nil
+}
+
+func (d DeppySolver) Solve(ctx context.Context) (Solution, error) {
+	querier := entitysource.NewGroup(d.entitySources...)
+	vars, err := d.constraintBuilder.Variables(ctx, querier)
+	if err != nil {
+		return nil, err
+	}
+
+	satSolver, err := sat.NewSolver(sat.WithInput(vars))
+	if err != nil {
+		return nil, err
+	}
+
+	selection, err := satSolver.Solve(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	solution := Solution{}
+	for _, variable := range vars {
+		if entity := querier.Get(ctx, entitysource.EntityID(variable.Identifier())); entity != nil {
+			solution[entity.ID()] = false
+		}
+	}
+	for _, variable := range selection {
+		if entity := querier.Get(ctx, entitysource.EntityID(variable.Identifier())); entity != nil {
+			solution[entity.ID()] = true
+		}
+	}
+	return solution, nil
+}
