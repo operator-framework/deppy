616e9e75c14389ca9657d38c1197f76a56722df0
perdasilva <perdasilva@redhat.com>
2022-10-24 11:20:27 +0200

    Add constraints package with basic constraint builder/generator with interfaces and basic implementations
    Signed-off-by: perdasilva <perdasilva@redhat.com>

---
+++ internal/constraints/constraint_generator.go
@@ -0,0 +1,40 @@
+package constraints
+
+import (
+	"context"
+
+	"github.com/operator-framework/deppy/internal/entitysource"
+	"github.com/operator-framework/deppy/internal/sat"
+)
+
+// ConstraintGenerator generates solver constraints given an entity querier interface
+type ConstraintGenerator interface {
+	GetVariables(ctx context.Context, querier entitysource.EntityQuerier) ([]sat.Variable, error)
+}
+
+var _ ConstraintGenerator = &ConstraintAggregator{}
+
+// ConstraintAggregator is a simple structure that aggregates different constraint generators
+// and collects all generated solver constraints
+type ConstraintAggregator struct {
+	constraintGenerators []ConstraintGenerator
+}
+
+func NewConstraintAggregator(constraintGenerators ...ConstraintGenerator) *ConstraintAggregator {
+	return &ConstraintAggregator{
+		constraintGenerators: constraintGenerators,
+	}
+}
+
+func (b *ConstraintAggregator) GetVariables(ctx context.Context, entityQuerier entitysource.EntityQuerier) ([]sat.Variable, error) {
+	// TODO: refactor to scatter cather through go routines
+	variables := make([]sat.Variable, 0)
+	for _, constraintGenerator := range b.constraintGenerators {
+		vars, err := constraintGenerator.GetVariables(ctx, entityQuerier)
+		if err != nil {
+			return nil, err
+		}
+		variables = append(variables, vars...)
+	}
+	return variables, nil
+}
---
+++ internal/constraints/variable.go
@@ -0,0 +1,30 @@
+package constraints
+
+import "github.com/operator-framework/deppy/internal/sat"
+
+var _ sat.Variable = &Variable{}
+
+// Variable is a simple implementation of sat.Variable
+type Variable struct {
+	id          sat.Identifier
+	constraints []sat.Constraint
+}
+
+func (v *Variable) Identifier() sat.Identifier {
+	return v.id
+}
+
+func (v *Variable) Constraints() []sat.Constraint {
+	return v.constraints
+}
+
+func (v *Variable) AddConstraint(constraint ...sat.Constraint) {
+	v.constraints = append(v.constraints, constraint...)
+}
+
+func NewVariable(id sat.Identifier, constraints ...sat.Constraint) *Variable {
+	return &Variable{
+		id:          id,
+		constraints: constraints,
+	}
+}
