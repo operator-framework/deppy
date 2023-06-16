30f47d692c5c632eb98eee050c602108e4fa57f2
Mikalai Radchuk <mradchuk@redhat.com>
2023-06-16 15:44:22 +0100

    Adds `ginkgolinter`
    Signed-off-by: Mikalai Radchuk <mradchuk@redhat.com>

--- cmd/dimacs/dimacs_test.go
+++ cmd/dimacs/dimacs_test.go
@@ -19,17 +19,17 @@ var _ = Describe("Dimacs", func() {
 	It("should fail if there is no header", func() {
 		problem := "1 2 3 0\n"
 		_, err := dimacs.NewDimacs(bytes.NewReader([]byte(problem)))
-		Expect(err).ToNot(BeNil())
+		Expect(err).To(HaveOccurred())
 	})
 	It("should fail if there are no clauses", func() {
 		problem := "p cnf 3 3\n"
 		_, err := dimacs.NewDimacs(bytes.NewReader([]byte(problem)))
-		Expect(err).ToNot(BeNil())
+		Expect(err).To(HaveOccurred())
 	})
 	It("should parse valid dimacs", func() {
 		problem := "p cnf 3 1\n1 2 3 0\n"
 		d, err := dimacs.NewDimacs(bytes.NewReader([]byte(problem)))
-		Expect(err).To(BeNil())
+		Expect(err).ToNot(HaveOccurred())
 		Expect(d.Variables()).To(Equal([]string{"1", "2", "3"}))
 		Expect(d.Clauses()).To(Equal([]string{"1 2 3"}))
 	})
